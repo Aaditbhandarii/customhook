64dd002320f567044deb95edb6ce285a80d32c90
Aaditbhandarii <AADIT.BHANDARI267@svkmmumbai.onmicrosoft.com>
2024-05-13 22:53:22 +0530

    final commit message

--- src/useCustomHook.jsx
+++ src/useCustomHook.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const useCustomHook = (initialCount) => {
   const [count, setCount] = useState(initialCount);
-  const increment = () => setCount(count + 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
   return [count, { increment, decrement, reset }];
